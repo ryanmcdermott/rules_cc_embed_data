225c71640c7644f1f4113b3c26a76b5ac004701c
Ryan McDermott <ryantmcdermott@gmail.com>
2023-08-20 09:24:34 -0700

    Move text files

--- examples/hello_world/hello_world.cc
+++ examples/hello_world/hello_world.cc
@@ -1,9 +1,9 @@
 #include <iostream>
-#include "examples/hello_world/example_texts.h"
+#include "examples/hello_world/text/example_texts.h"
 
 int main()
 {
-    std::cout << examples::hello_world::example1_txt() << "\n";
-    std::cout << examples::hello_world::example2_txt() << "\n";
+    std::cout << examples::hello_world::text::example1_txt() << "\n";
+    std::cout << examples::hello_world::text::example2_txt() << "\n";
     return 0;
 }
