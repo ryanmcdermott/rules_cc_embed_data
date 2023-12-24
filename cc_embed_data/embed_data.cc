5f3c09bfd4af01fd1b6f1a2e25015b6aa6f98784
Ryan McDermott <ryantmcdermott@gmail.com>
2023-12-23 21:07:49 -0800

    Add size function

--- cc_embed_data/embed_data.cc
+++ cc_embed_data/embed_data.cc
@@ -9,23 +9,27 @@ int main(int argc, char* argv[]) {
   std::string output_path = argv[1];
   std::string ns = argv[2];
   std::ofstream output_file(output_path);
+  output_file << "#include <cstdint>\n";
   output_file << "namespace " << ns << " {\n";
   for (int i = 3; i < argc; i += 2) {
     std::string input_path = argv[i];
     std::string var_name = argv[i + 1];
-    // Handle hyphenated files.
     std::replace(var_name.begin(), var_name.end(), '-', '_');
     std::ifstream input_file(input_path, std::ios::binary);
     output_file << "const char data_" << var_name << "[] = {";
     char ch;
+    int64_t byte_count = 0;
     while (input_file.get(ch)) {
       output_file << "0x" << std::hex << std::setw(2) << std::setfill('0')
                   << static_cast<unsigned int>(static_cast<unsigned char>(ch))
                   << ',';
+      byte_count++;
     }
     output_file << "0x00};\n";  // null terminator
     output_file << "const char* " << var_name << "() { return data_" << var_name
                 << "; }\n";
+    output_file << "uint64_t " << var_name << "_size() { return "
+                << std::to_string(byte_count) << "; }\n";
   }
   output_file << "}\n";
   return 0;
